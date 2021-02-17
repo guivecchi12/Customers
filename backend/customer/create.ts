f14495101fec64b99d533ff60ae31ac54339b157
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-02-16 17:09:24 -0700

    backend

--- backend/customer/create.ts
+++ backend/customer/create.ts
@@ -2,41 +2,16 @@ import {APIGatewayProxyEvent, APIGatewayProxyResult, Context} from "aws-lambda"
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
     const customer = JSON.parse(event.body!);
-
-    if(!customer){
-        return apiResponses._400({message: "You did not create a new customer"})
-    }
-
-    const customerData: {[key: string]: CustomerData} = {
-        customer1: {
-            id: customer.id,
-            name: customer.name,
-            email: customer.email,
-            about: customer.about
-        },
-    }
-    return apiResponses._200(customerData)
-}
-
-
-const apiResponses = {
-    _200: (body: {[key: string]: any}) => {
-        return {
-            statusCode: 200,
-            body: JSON.stringify(body, null, 2)
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message:'Hello from Lambda!', input:event
+        }),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Methods": "OPTIONS,POST"
         }
-    },
-    _400: (body: {[key: string]: any}) => {
-        return {
-            statusCode: 200,
-            body: JSON.stringify(body, null, 2)
-        }
-    }
-}
+    };
+    return response
 
-interface CustomerData{
-    id: number;
-    name: string;
-    email: string;
-    about?: string;
 }
